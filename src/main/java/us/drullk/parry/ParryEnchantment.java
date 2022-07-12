e6b9192a3dbcb7b3eb56cadf5dbfe73e61b39191
Drullkus <failbookftw@gmail.com>
2022-07-12 04:06:57 -0700

    1.19 port!

--- src/main/java/us/drullk/parry/ParryEnchantment.java
+++ src/main/java/us/drullk/parry/ParryEnchantment.java
@@ -1,28 +1,27 @@
 package us.drullk.parry;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.Registry;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ShieldItem;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraftforge.common.ToolActions;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegisterEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ParryEnchantment {
     static Enchantment reboundEnchantment;
-    private static final String enchantmentName = "rebound";
     private static EnchantmentCategory enchantmentTypeShield;
 
     @SubscribeEvent
-    public static void registerEnchantment(RegistryEvent.Register<Enchantment> event) {
+    public static void registerEnchantment(RegisterEvent event) {
         enchantmentTypeShield = EnchantmentCategory.create("shield", input -> input instanceof ShieldItem || input != null && input.canPerformAction(new ItemStack(input, 1), ToolActions.SHIELD_BLOCK));
 
-        reboundEnchantment = new EnchantmentRebound(Enchantment.Rarity.COMMON).setRegistryName(new ResourceLocation(ShieldParry.MODID, enchantmentName));
-        event.getRegistry().register(reboundEnchantment);
+        reboundEnchantment = new EnchantmentRebound(Enchantment.Rarity.COMMON);
+        event.register(Registry.ENCHANTMENT_REGISTRY, ShieldParry.modId("rebound"), () -> reboundEnchantment);
     }
 
     private static class EnchantmentRebound extends Enchantment {
