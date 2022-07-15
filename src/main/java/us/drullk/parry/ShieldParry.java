37eca6615a8446e834def193b83f937831d0908c
Drullkus <failbookftw@gmail.com>
2022-07-14 20:24:24 -0700

    Simplify this logic that aged faster than milk, good lord

--- src/main/java/us/drullk/parry/ShieldParry.java
+++ src/main/java/us/drullk/parry/ShieldParry.java
@@ -20,14 +20,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.function.BiConsumer;
-
 @Mod(ShieldParry.MODID)
 @Mod.EventBusSubscriber(modid = ShieldParry.MODID)
 public class ShieldParry {
     public static final String MODID = "parry";
     public static final TagKey<EntityType<?>> PROJECTILES_DISABLED_FOR_PARRYING = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, ShieldParry.modId("projectiles_parrying_disabled"));
-    //private static Logger LOGGER = LogManager.getLogger(ShieldParry.MODID);
 
     public ShieldParry() {
         Pair<ParryConfig, ForgeConfigSpec> pairConfigSpec = new ForgeConfigSpec.Builder().configure(ParryConfig::new);
@@ -35,14 +32,22 @@ public class ShieldParry {
         ParryConfig.INSTANCE = pairConfigSpec.getLeft();
     }
 
-    private static <T extends Projectile> boolean parryProjectile(T projectile, LivingEntity entityBlocking, boolean takeOwnership, BiConsumer<Vec3, T> trajectoryChange) {
+    private static <T extends Projectile> boolean parryProjectile(T projectile, LivingEntity entityBlocking, boolean takeOwnership) {
         if (!projectile.getType().is(PROJECTILES_DISABLED_FOR_PARRYING) && entityBlocking.isBlocking() && entityBlocking.getUseItem().getUseDuration() - entityBlocking.getUseItemRemainingTicks() <= applyTimerBonus(ParryConfig.INSTANCE.shieldParryTicks.get(), entityBlocking.getUseItem(), ParryConfig.INSTANCE.shieldEnchantmentMultiplier.get())) {
             if (takeOwnership) {
                 projectile.setOwner(entityBlocking);
                 projectile.leftOwner = true;
             }
 
-            trajectoryChange.accept(entityBlocking.getLookAngle(), projectile);
+            Vec3 reboundAngle = entityBlocking.getLookAngle();
+
+            projectile.shoot(reboundAngle.x, reboundAngle.y, reboundAngle.z, 1.1F, 0.1F);  // reflect faster and more accurately
+
+            if (projectile instanceof AbstractHurtingProjectile damagingProjectile) {
+                damagingProjectile.xPower = reboundAngle.x * 0.1D;
+                damagingProjectile.yPower = reboundAngle.y * 0.1D;
+                damagingProjectile.zPower = reboundAngle.z * 0.1D;
+            }
 
             return true;
         }
@@ -50,23 +55,16 @@ public class ShieldParry {
         return false;
     }
 
-    private static boolean dispatchShieldParry(Projectile projectile, EntityHitResult entityHitResult, boolean takeOwnership, BiConsumer<Vec3, Projectile> trajectoryChange) {
-        return !projectile.level.isClientSide()
-                && entityHitResult.getEntity() instanceof LivingEntity livingEntity
-                && parryProjectile(projectile, livingEntity, takeOwnership, trajectoryChange);
-    }
-
     @SubscribeEvent
     public static void parryThisCasual(ProjectileImpactEvent event) {
-        if (event.getEntity() instanceof Projectile projectile && event.getRayTraceResult() instanceof EntityHitResult entityHitResult && dispatchShieldParry(projectile, entityHitResult, !(event.getEntity() instanceof FishingHook), (reboundAngle, rebounding) -> {
-            rebounding.shoot(reboundAngle.x, reboundAngle.y, reboundAngle.z, 1.1F, 0.1F);  // reflect faster and more accurately
-
-            if (rebounding instanceof AbstractHurtingProjectile damagingProjectile) {
-                damagingProjectile.xPower = reboundAngle.x * 0.1D;
-                damagingProjectile.yPower = reboundAngle.y * 0.1D;
-                damagingProjectile.zPower = reboundAngle.z * 0.1D;
-            }
-        })) event.setCanceled(true);
+        if (!event.getEntity().getLevel().isClientSide()
+                && event.getEntity() instanceof Projectile projectile
+                && event.getRayTraceResult() instanceof EntityHitResult entityHitResult
+                && entityHitResult.getEntity() instanceof LivingEntity livingEntity
+                && parryProjectile(projectile, livingEntity, !(projectile instanceof FishingHook))
+        ) {
+            event.setCanceled(true);
+        }
     }
 
     private static int applyTimerBonus(int base, ItemStack stack, double multiplier) {
