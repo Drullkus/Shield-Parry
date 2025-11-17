7e482456ab8e66325cae9ccb0fffb6032e21080a
Drullkus <5010174+Drullkus@users.noreply.github.com>
2025-11-16 23:07:45 -0800

    Add sound and particles for rebound

--- src/main/java/us/drullk/parry/ShieldParry.java
+++ src/main/java/us/drullk/parry/ShieldParry.java
@@ -2,16 +2,22 @@ package us.drullk.parry;
 
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.tags.TagKey;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.*;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.phys.EntityHitResult;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
@@ -41,37 +47,61 @@ public class ShieldParry {
         modEventBus.addListener(ParryDatagen::generateData);
     }
 
+	private void parryThisCasual(ProjectileImpactEvent event) {
+		if (!event.getEntity().level().isClientSide()
+				&& event.getEntity() instanceof Projectile projectile
+				&& event.getRayTraceResult() instanceof EntityHitResult entityHitResult
+				&& entityHitResult.getEntity() instanceof LivingEntity livingEntity
+				&& this.tryParry(projectile, livingEntity)
+		) {
+			event.setCanceled(true);
+		}
+	}
+
     private <T extends Projectile> boolean tryParry(T projectile, LivingEntity entityBlocking) {
-        if (!projectile.getType().is(PROJECTILES_DISABLED_FOR_PARRYING) && entityBlocking.isBlocking() && projectile.getOwner() != entityBlocking) {
-            ItemStack itemUsed = entityBlocking.getUseItem();
-            if (!itemUsed.is(EXCLUDED_SHIELDS) && itemUsed.getUseDuration(entityBlocking) - entityBlocking.getUseItemRemainingTicks() <= getTimerBonus(entityBlocking.registryAccess(), this.config.shieldParryTicks.get(), itemUsed, this.config.shieldEnchantmentMultiplier.get())) {
-                return projectile.deflect(ProjectileDeflection.AIM_DEFLECT, entityBlocking, projectile.getOwner(), true);
-            }
-        }
+		if (projectile.getType().is(PROJECTILES_DISABLED_FOR_PARRYING) || !entityBlocking.isBlocking() || projectile.getOwner() == entityBlocking) {
+			return false;
+		}
 
-        return false;
-    }
+        ItemStack itemUsed = entityBlocking.getUseItem();
+		if (itemUsed.is(EXCLUDED_SHIELDS))
+			return false;
 
-    private void parryThisCasual(ProjectileImpactEvent event) {
-        if (!event.getEntity().level().isClientSide()
-                && event.getEntity() instanceof Projectile projectile
-                && event.getRayTraceResult() instanceof EntityHitResult entityHitResult
-                && entityHitResult.getEntity() instanceof LivingEntity livingEntity
-                && this.tryParry(projectile, livingEntity)
-        ) {
-            event.setCanceled(true);
+		int timeUp = itemUsed.getUseDuration(entityBlocking) - entityBlocking.getUseItemRemainingTicks();
+		int baseLimit = this.config.shieldParryTicks.get();
+		int limitToCrit = getReboundDuration(entityBlocking.registryAccess(), baseLimit, itemUsed, this.config.shieldEnchantmentMultiplier.get());
+		// Cannot rebound if shield up longer than limit
+		if (timeUp > limitToCrit) {
+			return false;
+		}
+
+		if (entityBlocking.level() instanceof ServerLevel serverLevel) {
+			boolean isEnchantBoosted = timeUp <= baseLimit;
+			this.sendEffects(entityBlocking, serverLevel, isEnchantBoosted, projectile.getX(), projectile.getY(), projectile.getZ());
+		}
+
+		return projectile.deflect(ProjectileDeflection.AIM_DEFLECT, entityBlocking, projectile.getOwner(), true);
+	}
+
+	private void sendEffects(LivingEntity entityBlocking, ServerLevel serverLevel, boolean isEnchantBoosted, double effectX, double effectY, double effectZ) {
+        serverLevel.playSeededSound(null, effectX, effectY, effectZ, isEnchantBoosted ? SoundEvents.PLAYER_ATTACK_SWEEP : SoundEvents.PLAYER_ATTACK_CRIT, SoundSource.PLAYERS, 1f, 1f, entityBlocking.getRandom().nextInt());
+		ParticleOptions particleType = isEnchantBoosted ? ParticleTypes.ENCHANTED_HIT : ParticleTypes.CRIT;
+		if (entityBlocking instanceof ServerPlayer playerBlocking) {
+            serverLevel.sendParticles(playerBlocking, particleType, false, effectX, effectY, effectZ, 3, 0.1, 0.1, 0.1, 0.1);
+        } else {
+            serverLevel.sendParticles(particleType, effectX, effectY, effectZ, 3, 0.1, 0.1, 0.1, 0.1);
         }
     }
 
     @SuppressWarnings("OptionalIsPresent")
-	private static int getTimerBonus(RegistryAccess registryAccess, int base, ItemStack stack, double multiplier) {
+	private static int getReboundDuration(RegistryAccess registryAccess, int base, ItemStack stack, double multiplier) {
         Optional<Holder.Reference<Enchantment>> reboundEnchantment = registryAccess.registry(Registries.ENCHANTMENT).flatMap(r -> r.getHolder(ParryEnchantments.REBOUND));
 
         if (reboundEnchantment.isEmpty()) {
             return 0;
         }
 
-        return (int) (base + base * EnchantmentHelper.getItemEnchantmentLevel(reboundEnchantment.get(), stack) * multiplier);
+		return Mth.ceil(base + base * stack.getEnchantmentLevel(reboundEnchantment.get()) * multiplier);
     }
 
     public static ResourceLocation modId(String name) {
